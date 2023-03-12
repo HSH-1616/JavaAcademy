b93050d5abdcd4b496277b405c5a2435e0911a72
HSH-1616 <hgfgf1116@gmail.com>
2023-03-12 15:05:41 +0900

    1

--- Practice/src/com/rsp/RSP.java
+++ Practice/src/com/rsp/RSP.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class RSP {
-	
+	//dd
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
 	Random r=new Random();
