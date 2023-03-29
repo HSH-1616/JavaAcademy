4bbcd4b7ea29b139a7f2a668c36a131e1ebd00ac
HSH-1616 <hgfgf1116@gmail.com>
2023-03-29 19:50:58 +0900

    1

--- JavaWork/score/model/dao/ScoreDAO.java
+++ JavaWork/score/model/dao/ScoreDAO.java
@@ -5,44 +5,26 @@ import java.io.DataOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class ScoreDAO {
-
 	public void saveScore(String name, int kor, int eng, int math, int sum, double avg) {
-
-		try (FileOutputStream fos = new FileOutputStream("StudentScore");
-				DataOutputStream dos = new DataOutputStream(fos);) {
-
+		try(FileOutputStream fos = new FileOutputStream("./Score.txt", true);
+			DataOutputStream dos = new DataOutputStream(fos)) {
 			dos.writeUTF(name);
 			dos.writeInt(kor);
 			dos.writeInt(eng);
 			dos.writeInt(math);
 			dos.writeInt(sum);
 			dos.writeDouble(avg);
-		} catch (IOException e) {
+		} catch(IOException e) {
 			e.printStackTrace();
-		}
+		} 
 	}
-
+	
 	public DataInputStream readScore() throws FileNotFoundException {
-		FileInputStream fis = new FileInputStream("StudentScore");
-		DataInputStream dis = new DataInputStream(fis);
-		try {
-			String name = dis.readUTF();
-			int kor = dis.readInt();
-			int eng = dis.readInt();
-			int math = dis.readInt();
-			int sum = dis.readInt();
-			double avg = dis.readDouble();
-
-			
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+		DataInputStream dis = new DataInputStream(new FileInputStream("./Score.txt"));
 		return dis;
-
 	}
-
 }
