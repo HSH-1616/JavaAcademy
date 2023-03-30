5aa25424eceae4243ba3e55d41ad166fe46bc304
HSH-1616 <hgfgf1116@gmail.com>
2023-03-30 22:14:03 +0900

    1

--- JavaWork/Homeworks10_Collection/src/com/bs/practice/list/library/controller/BookController.java
+++ JavaWork/Homeworks10_Collection/src/com/bs/practice/list/library/controller/BookController.java
@@ -2,6 +2,8 @@ package com.bs.practice.list.library.controller;
 
 import java.util.ArrayList;
 
+import javax.xml.validation.Validator;
+
 import com.bs.practice.list.library.model.vo.Book;
 
 public class BookController {
@@ -9,11 +11,35 @@ public class BookController {
 	ArrayList list = new ArrayList();
 	
 	public BookController() {
-		// TODO Auto-generated constructor stub
+		list.add(new Book("자바의 정석","남궁성","기타",20000));
+		list.add(new Book("쉽게 배우는 알고리즘","문병로","기타",15000));
+		list.add(new Book("대화의 기술","강보람","인문",17500));
+		list.add(new Book("암 정복기","박신우","의료",21000));
 	}
 	
 	public void insertBook(Book bk) {
-		list.add(new Book(bk.title,bk.author,bk.category,bk.price));
-		
+		list.add(new Book(bk.title,bk.author,bk.category,bk.price));		
 	}
+	
+	public ArrayList selectList() {
+		return list;
+	}
+	
+	public ArrayList searchBook(String keyword) {
+		ArrayList searchList=new ArrayList();
+		
+		for(int i=0;i<list.size();i++) {
+			for(int j=0;j<=((Book)list.get(i)).getTitle().length();j++){
+				if(String.valueOf(((Book)list.get(i)).getTitle().charAt(j)).equals(keyword)) {
+					searchList.add(list.get(i));
+				}
+			}
+									
+		}
+		
+		
+		
+		return searchList;
+	}
+	
 }
--- JavaWork/Homeworks10_Collection/src/com/bs/practice/list/library/view/BookMenu.java
+++ JavaWork/Homeworks10_Collection/src/com/bs/practice/list/library/view/BookMenu.java
@@ -1,5 +1,6 @@
 package com.bs.practice.list.library.view;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import com.bs.practice.list.library.controller.BookController;
@@ -18,10 +19,19 @@ public class BookMenu {
 			System.out.println("******* 메인 메뉴 *******");
 			System.out.println("1. 새 도서 추가\n2. 도서 전체 조회\n3. 도서 검색 조회\n4. 도서 삭제\n5 .도서 명 오름차순 정렬\n9. 종료");
 			System.out.print("메뉴 번호 선택 : ");
+			
 			menuNum = sc.nextInt();
-
+			sc.nextLine();
 			switch (menuNum) {
 			case 1:
+				insertBook();
+				break;
+			case 2:
+				selectList();
+				break;
+			case 3:
+				searchBook();
+				break;
 
 			case 9:
 				System.out.println("프로그램을 종료합니다.");
@@ -36,11 +46,11 @@ public class BookMenu {
 	public void insertBook() {
 		System.out.print("도서 명 : ");
 		String title = sc.next();
-		System.out.println("저자 명 : ");
+		System.out.print("저자 명 : ");
 		String author = sc.next();
-		System.out.println("장르(1. 인문 / 2. 과학 / 3. 외국어 / 4. 기타) : ");
+		System.out.print("장르(1. 인문 / 2. 과학 / 3. 외국어 / 4. 기타) : ");
 		int category = sc.nextInt();
-		System.out.println("가격 : ");
+		System.out.print("가격 : ");
 		int price = sc.nextInt();
 
 		String categorys = "";
@@ -61,8 +71,27 @@ public class BookMenu {
 
 		Book bk = new Book(title, author, categorys, price);
 		bc.insertBook(bk);
+	}
+
+	public void selectList() {
+		ArrayList<Book> booklist = bc.selectList();
+
+		if (booklist.isEmpty()) {
+			System.out.println("검색 결과가 없습니다.");
+		} else {
+			booklist.forEach(o -> System.out.println(o));
+		}
+	}
+	
+	public void searchBook() {
+		System.out.print("검색 키워드 : ");
+		String keyword=sc.nextLine();
 		
-		
+		if(bc.searchBook(keyword).isEmpty()) {
+			System.out.println("검색 결과가 없습니다.");
+		}else {
+			bc.searchBook(keyword).forEach(o->System.out.println(o));
+		}
 	}
 
 }
