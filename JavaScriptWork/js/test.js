d909d4219385d67c4bdbaa603d86aafc5e7860ad
HSH-1616 <hgfgf1116@gmail.com>
2023-05-03 17:35:36 +0900

    1

--- JavaScriptWork/js/test.js
+++ JavaScriptWork/js/test.js
@@ -1,5 +1,9 @@
-alert("external로 실행하기");
+// alert("external로 실행하기");
 
 const testfunc = () => {
   alert("testfunc함수를 실행하기");
 };
+
+let outerTest = 100;
+let outerTest2 = [10, 20, 30, 40];
+var outerTest3 = 300;
