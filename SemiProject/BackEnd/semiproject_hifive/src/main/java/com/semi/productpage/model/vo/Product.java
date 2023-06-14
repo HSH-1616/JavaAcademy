591002b117e6f6d2386b18bdb2004862657ce643
HSH-1616 <hgfgf1116@gmail.com>
2023-06-15 01:43:35 +0900

    1

---
+++ SemiProject/BackEnd/semiproject_hifive/src/main/java/com/semi/productpage/model/vo/Product.java
@@ -0,0 +1,27 @@
+package com.semi.productpage.model.vo;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Product {
+	private int productId;
+	private String userId;
+	private String title;
+	private String status;
+	private String sellStatus;
+	private int price;
+	private Date registTime;
+	private int viewCount;
+	private String explanation;
+	private String keyword;
+	private String categoryId;
+	private int areaId;
+}
