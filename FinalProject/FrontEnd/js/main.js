e310b2a8573e30b51ae7f9322f9443edfb4da0e6
HSH-1616 <hgfgf1116@gmail.com>
2023-07-12 02:01:07 +0900

    1

--- FinalProject/FrontEnd/js/main.js
+++ FinalProject/FrontEnd/js/main.js
@@ -1,3 +1,9 @@
+$(window).ready(function () {
+  $(".btn").css("background-color", "transparent");
+});
+
+AOS.init();
+
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
   var height = $(".navbar").height();
@@ -8,6 +14,14 @@ $(window).scroll(function () {
   }
 });
 
-$(window).ready(function () {
-  $(".btn").css("background-color", "transparent");
+$(".main-menu").mouseover(function () {
+  $(".navbar").addClass("drop");
+});
+
+$(".main-menu").mouseout(function () {
+  var scroll = $(window).scrollTop();
+  var height = $(".navbar").height();
+  if (scroll < height) {
+    $(".navbar").removeClass("drop");
+  }
 });
